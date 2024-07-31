c45860e3fe0e9b5dd07ecb0ad21594d28eee33a0
Roman3340 <roman.chukaev1@gmail.com>
2024-07-31 16:38:28 +0700

    autocar3

--- autocomplete.js
+++ autocomplete.js
@@ -1,3 +1,13 @@
+function cyrillicToLatin(text) {
+    const cyrillicMap = {
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'YO', 'Ж': 'ZH', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'KH', 'Ц': 'TS', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 'Ь': '', 'Ы': 'Y', 'Ъ': '', 'Э': 'E', 'Ю': 'YU', 'Я': 'YA',
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'
+    };
+    
+    return text.split('').map(char => cyrillicMap[char] || char).join('');
+}
+
+
 $(document).ready(function() {
     // Замените этот URL на путь к вашему JSON-файлу
     const carDataUrl = 'cars.json'; 
@@ -19,7 +29,24 @@ $(document).ready(function() {
         // Инициализация автозаполнения
         $('#car').autocomplete({
             source: carBrandsAndModels,
-            minLength: 2 // Минимальная длина ввода для начала поиска
+            minLength: 2, // Минимальная длина ввода для начала поиска
+            open: function(event, ui) {
+                const input = $(this).val();
+                const translatedInput = cyrillicToLatin(input);
+                $(this).val(translatedInput);
+            },
+            select: function(event, ui) {
+                $(this).val(ui.item.value);
+                return false; // Предотвращает изменение значения
+            }
+        });
+
+        // Обработчик ввода для перевода на латиницу и поиска
+        $('#car').on('input', function() {
+            const input = $(this).val();
+            const translatedInput = cyrillicToLatin(input);
+            $(this).val(translatedInput);
+            $('#car').autocomplete('search', translatedInput);
         });
     });
 });
